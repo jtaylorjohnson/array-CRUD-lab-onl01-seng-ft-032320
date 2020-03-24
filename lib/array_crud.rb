9b20f1fabda4ff1b6bb2f3b1fa5bcae7c6c63b5c
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-24 22:50:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   animals = ["turkey", "pig"]
-  animals << "duck"
+  animals << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
